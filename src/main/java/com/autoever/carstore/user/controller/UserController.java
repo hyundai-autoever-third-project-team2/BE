a73017ff1643dfe602e201026f02e59b8c6c48b3
오인우 <oin-u@oin-us-iMac.local>
2025-01-14 15:48:14 +0900

    feat: 차량 판매 내역 조회 및 찜한 차량 조회 api 개발 완료

--- src/main/java/com/autoever/carstore/user/controller/UserController.java
+++ src/main/java/com/autoever/carstore/user/controller/UserController.java
@@ -2,7 +2,9 @@ package com.autoever.carstore.user.controller;
 
 import com.autoever.carstore.car.entity.CarSalesEntity;
 import com.autoever.carstore.car.service.CarService;
+import com.autoever.carstore.user.dto.response.IsHeartCarResponseDto;
 import com.autoever.carstore.user.dto.response.TransactionStatusResponseDto;
+import com.autoever.carstore.user.dto.response.UserCarTransactionStatusResponseDto;
 import com.autoever.carstore.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +29,22 @@ public class UserController {
     ) {
         long userId = 5;
         List<TransactionStatusResponseDto> result = carService.viewTransaction(userId, progress);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/userCarTransaction")
+    public ResponseEntity<List<UserCarTransactionStatusResponseDto>> userCarTransaction(
+            @RequestParam String progress
+    ){
+        long userId = 5;
+        List<UserCarTransactionStatusResponseDto> result = carService.viewUserCarTransaction(userId, progress);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/isHeartCar")
+    public ResponseEntity<List<IsHeartCarResponseDto>> isHeartCar(){
+        long userId = 5;
+        List<IsHeartCarResponseDto> result = carService.viewIsHeartCar(userId);
         return ResponseEntity.ok(result);
     }
 }
