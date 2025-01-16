343f403c973f7ebf0f29a65a2503f78329d99517
JunHyuk Jang <12161782@inha.edu>
2025-01-17 01:24:10 +0900

    feat: 알림 테스트 완료 -> 기능과 연동 필요 #29

--- src/main/java/com/autoever/carstore/fcm/controller/FCMController.java
+++ src/main/java/com/autoever/carstore/fcm/controller/FCMController.java
@@ -1,6 +1,6 @@
 package com.autoever.carstore.fcm.controller;
 
-import com.autoever.carstore.fcm.FCMUtil;
+//import com.autoever.carstore.fcm.FCMUtil;
 import com.autoever.carstore.fcm.dto.FCMRequestDto;
 import com.autoever.carstore.fcm.service.FCMService;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class FCMController {
     private final FCMService fcmService;
-    private final FCMUtil fcmUtil;
-    private final String fcmToken = "cAZ0rEnFTamu_pXJFV9iqg:APA91bGDxYY3u2DqfJG1_3o-QXa7TOymv8Mg4wZKZeXTrBdaRHXfOxKs00ZRA1YE8NFxJri30JJwldTe3IZhMUWmITpzhki5sgYHyVOkPxPync96bBPBEMk";
+//    private final FCMUtil fcmUtil;
+//    private final String fcmToken = "cAZ0rEnFTamu_pXJFV9iqg:APA91bGDxYY3u2DqfJG1_3o-QXa7TOymv8Mg4wZKZeXTrBdaRHXfOxKs00ZRA1YE8NFxJri30JJwldTe3IZhMUWmITpzhki5sgYHyVOkPxPync96bBPBEMk";
 
     @PostMapping("/send")
     public ResponseEntity<String> pushMessage(@RequestBody FCMRequestDto requestDTO) throws Exception {
