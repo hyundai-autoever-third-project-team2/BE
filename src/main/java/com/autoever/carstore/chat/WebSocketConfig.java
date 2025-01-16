16a0933df83385267d5b9ca43ee4036f20d0ef3d
inwoo0808 <inwoo0808@gmail.com>
2025-01-16 10:16:48 +0900

    feat: 실시간 채팅 api 1차 구현

--- src/main/java/com/autoever/carstore/chat/WebSocketConfig.java
+++ src/main/java/com/autoever/carstore/chat/WebSocketConfig.java
@@ -1,24 +1,24 @@
 package com.autoever.carstore.chat;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
 
 @Configuration
-@EnableWebSocket
-@RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketConfigurer {
-
-    private final WebSocketChatHandler webSocketHandler;
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry
-                // /ws/conn 경로로 WebSocket 연결을 허용
-                .addHandler(webSocketHandler, "/ws/conn")
-                // CORS 허용
-                .setAllowedOrigins("*");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
     }
 }
