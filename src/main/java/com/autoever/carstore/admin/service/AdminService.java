eb03461b2b01fe6c570341a586551e79320e3d31
eunsol <eunsol037@naver.com>
2025-01-16 17:49:17 +0900

    feat : admin 페이지 거래 로직 추가

--- src/main/java/com/autoever/carstore/admin/service/AdminService.java
+++ src/main/java/com/autoever/carstore/admin/service/AdminService.java
@@ -1,27 +1,53 @@
 package com.autoever.carstore.admin.service;
 
-import com.autoever.carstore.user.entity.UserEntity;
+import com.autoever.carstore.car.dao.CarSalesRepository;
+import com.autoever.carstore.car.entity.CarSalesEntity;
 import com.autoever.carstore.user.dao.UserRepository;
+import com.autoever.carstore.user.dto.response.TransactionStatusResponseDto;
+import com.autoever.carstore.user.dto.response.TransactionsResponseDto;
 import com.autoever.carstore.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class AdminService {
-    private final UserRepository userRepository;
-    private final UserService userService;
+    private final CarSalesRepository carSalesRepository;
 
-    public void toggleUserStatus(Long userId, boolean isActive) {
-        UserEntity user = userService.getUserById(userId); // 유저 조회
+    public List<TransactionsResponseDto> getAllRecentTransactions() {
+        List<CarSalesEntity> carSalesEntities = carSalesRepository.findAll();
+        List<TransactionsResponseDto> results = new ArrayList<>();
 
-        if (user == null) {
-            throw new IllegalArgumentException("User not found with ID: " + userId);
+        for (CarSalesEntity carSaleEntity : carSalesEntities) {
+            if (carSaleEntity.getUser() == null) { // 방어 코드 추가
+
+                continue; // 해당 레코드 스킵
+            }
+
+
+            for (CarSalesEntity carSalesEntity : carSalesEntities) {
+                TransactionsResponseDto transactionsResponseDto = TransactionsResponseDto.builder()
+                        .car_sales_id(carSalesEntity.getCarSalesId())
+                        .sales_date(carSalesEntity.getUpdatedAt())
+                        .progress(carSalesEntity.getProgress())
+                        .brand(carSalesEntity.getCar().getCarModel().getBrand())
+                        .model_name(carSalesEntity.getCar().getCarModel().getModelName())
+                        .order_number(carSalesEntity.getOrderNumber())
+                        .price(carSalesEntity.getPrice())
+                        .userId(carSalesEntity.getUser().getUserId()) // 유저 ID 추가
+                        .userName(carSalesEntity.getUser().getNickname()) // 유저 이름 추가
+                        .build();
+                results.add(transactionsResponseDto);
+            }
+
         }
+        return results;
 
-        user.updateIsActive(); // 상태 반전
-        userRepository.save(user); // 저장
     }
-
-
 }
