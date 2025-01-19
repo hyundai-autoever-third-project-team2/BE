d111fb871cded1a22d2435853ffb9c4a4114d6a1
JunHyuk Jang <12161782@inha.edu>
2025-01-19 16:07:47 +0900

    feat: change notification request #29

--- src/main/java/com/autoever/carstore/notification/controller/NotificationController.java
+++ src/main/java/com/autoever/carstore/notification/controller/NotificationController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/notification")
@@ -16,7 +17,9 @@ public class NotificationController {
     private final NotificationService notificationService;
 
     @PutMapping("/click")
-    public ResponseEntity<?> register(@RequestParam Long notificationId) {
+    public ResponseEntity<?> register(@RequestBody Map<String, Long> request) {
+        Long notificationId = request.get("notificationId");
+
         notificationService.clickNotification(notificationId);
 
         return ResponseEntity.ok("success");
