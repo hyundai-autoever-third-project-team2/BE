6ed389db546e4b0f31019b0da0a84167a507ec97
itcuna0617 <itcuna0617@gmail.com>
2025-01-20 20:58:01 +0900

    feat: 스케줄러 시간 변경

--- src/main/java/com/autoever/carstore/recommend/service/implement/RecommendServiceImplement.java
+++ src/main/java/com/autoever/carstore/recommend/service/implement/RecommendServiceImplement.java
@@ -35,7 +35,7 @@ public class RecommendServiceImplement implements RecommendService {
     @Override
   //  @Scheduled(cron = "0 0 0 * * MON")
 //    @Scheduled(cron = "0 11 18 * * MON")
-    @Scheduled(cron = "0 55 20 * * MON")
+    @Scheduled(cron = "0 02 21 * * MON")
     @Transactional
     public void updateRecommendations() {
         List<Long> userIds = recommendRepository.findAllUserIds();
